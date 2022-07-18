5d6ee218f6b771ec7c41a1f226b986b8edc3e7ef
Keks <keks@htmlacademy.ru>
2022-07-19 00:21:05 +0300

    :heavy_check_mark: Сборка #5

---
+++ 5/js/main.js
@@ -0,0 +1,7 @@
+import './util.js';
+import './data.js';
+import './browse.js';
+import './edit-photo.js';
+import './filtre.js';
+import './post-data.js';
+import './upload-photo.js';
---
+++ 5/js/upload-photo.js
@@ -0,0 +1,19 @@
+import {getArrUsersPhotos} from './data.js';
+
+
+const photoContainer = document.querySelector('.pictures');
+const userPictureTemplate = document.querySelector('#picture').content;
+const usersPhotos = getArrUsersPhotos();
+
+const userPhotoFragment = document.createDocumentFragment();
+
+usersPhotos.forEach(({likes, url, comment}) => {
+  const photoElement = userPictureTemplate.cloneNode(true);
+  photoElement.querySelector('.picture__likes').textContent = likes;
+  photoElement.querySelector('.picture__img').src = url;
+  photoElement.querySelector('.picture__comments').textContent = comment;
+  photoContainer.append(photoElement);
+});
+
+photoContainer.append(userPhotoFragment);
+
