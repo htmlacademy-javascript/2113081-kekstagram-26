798f744f0136f52111c4f5f5568c545ece2dd4d1
Keks <keks@htmlacademy.ru>
2022-07-25 18:50:24 +0300

    :heavy_check_mark: Сборка #5

--- 5/js/upload-photo.js
+++ 5/js/upload-photo.js
@@ -7,11 +7,12 @@ const usersPhotos = getArrUsersPhotos();
 
 const userPhotoFragment = document.createDocumentFragment();
 
+
 usersPhotos.forEach(({likes, url, comment}) => {
   const photoElement = userPictureTemplate.cloneNode(true);
   photoElement.querySelector('.picture__likes').textContent = likes;
   photoElement.querySelector('.picture__img').src = url;
-  photoElement.querySelector('.picture__comments').textContent = comment;
+  photoElement.querySelector('.picture__comments').textContent = comment.length;
   photoContainer.append(photoElement);
 });
 
